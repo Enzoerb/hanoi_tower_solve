39f4271ad775f6bd16c389ddde828b2561b0121a
Enzo Erbert <enzo.erbert@semantix.com.br>
2020-05-26 14:25:03 -0300

    solving recursively

--- solve.py
+++ solve.py
@@ -1,101 +1,54 @@
-from random import randint
 from game import HanoiTower
 
-Game = HanoiTower()
-Game.inicialize_game()
 
-print(str(Game))
-for _ in range(len(Game.rods[1])):
-    Game.change(1, 0)
-print(str(Game))
+class SolveHanoiTower:
 
-tries = 0
-while not Game.check_win():
+    def __init__(self, Game=HanoiTower(), inicialized=False):
+        self.Game = Game
+        self.higher = 0
+        self.lower = 2
+        if not inicialized:
+            self.Game.inicialize_game()
 
-    if len(Game.rods[1]) == 0:
-        if len(Game.rods[0]) > 0:
-            if len(Game.rods[2]) > 0:
-                if Game.rods[0][-1] > Game.rods[2][-1]:
-                    Game.change(0, 1)
-                else:
-                    Game.change(2, 1)
+    def higher_lower(self):
+        if len(self.Game.rods[0]) > 0 and len(self.Game.rods[2]) > 0:
+            self.lower, self.higher = (
+                2, 0) if self.Game.rods[0][-1] > self.Game.rods[2][-1] else (0, 2)
+        elif len(self.Game.rods[0]) > 0:
+            self.lower, self.higher = (2, 0)
+        elif len(self.Game.rods[2]) > 0:
+            self.lower, self.higher = (0, 2)
+
+    def clear_midrow(self):
+        for _ in range(len(self.Game.rods[1])):
+            self.Game.change(1, 0)
+
+    def higher_to_midrow(self):
+        if len(self.Game.rods[1]) > 0:
+            if self.Game.rods[1][-1] < self.Game.rods[self.higher][-1]:
+                self.Game.change(1, self.lower)
+                self.higher_to_midrow()
+                self.Game.change(self.lower, 1)
             else:
-                Game.change(0, 1)
-        elif len(Game.rods[2]) > 0:
-            Game.change(2, 1)
-    else:
-        if len(Game.rods[0]) > 0 and len(Game.rods[2]) > 0:
-            if Game.rods[1][-1] > Game.rods[0][-1] and Game.rods[1][-1] > Game.rods[2][-1]:
-                if Game.rods[0][-1] > Game.rods[2][-1]:
-                    Game.change(0, 1)
-                else:
-                    Game.change(2, 1)
-            else:
-                if Game.rods[0][-1] > Game.rods[2][-1]:
-                    centered_disks = len(Game.rods[1])
-                    for _ in range(centered_disks):
-                        Game.change(1, 2)
-                    for actual_disk in range(centered_disks):
-                        if Game.rods[0][-1] > Game.rods[2][-1]:
-                            Game.change(0, 1)
-                            for _ in range(centered_disks-actual_disk):
-                                Game.change(2, 1)
-                            break
-                        else:
-                            Game.change(2, 1)
-                else:
-                    centered_disks = len(Game.rods[1])
-                    for _ in range(centered_disks):
-                        Game.change(1, 0)
-                    for actual_disk in range(centered_disks):
-                        if Game.rods[2][-1] > Game.rods[0][-1]:
-                            Game.change(2, 1)
-                            for _ in range(centered_disks-actual_disk):
-                                Game.change(0, 1)
-                            break
-                        else:
-                            Game.change(0, 1)
-
-        elif len(Game.rods[0]) > 0:
-            if Game.rods[1][-1] > Game.rods[0][-1]:
-                Game.change(0, 1)
-            else:
-                centered_disks = len(Game.rods[1])
-                for _ in range(centered_disks):
-                    Game.change(1, 2)
-                for actual_disk in range(centered_disks):
-                    if Game.rods[0][-1] > Game.rods[2][-1]:
-                        Game.change(0, 1)
-                        for _ in range(centered_disks-actual_disk):
-                            Game.change(2, 1)
-                        break
-                    else:
-                        Game.change(2, 1)
-
-        elif len(Game.rods[2]) > 0:
-            if Game.rods[1][-1] > Game.rods[2][-1]:
-                Game.change(2, 1)
-            else:
-                centered_disks = len(Game.rods[1])
-                for _ in range(centered_disks):
-                    Game.change(1, 0)
-                for actual_disk in range(centered_disks):
-                    if Game.rods[2][-1] > Game.rods[0][-1]:
-                        Game.change(2, 1)
-                        for _ in range(centered_disks-actual_disk):
-                            Game.change(0, 1)
-                        break
-                    else:
-                        Game.change(0, 1)
-
+                self.Game.change(self.higher, 1)
         else:
-            break
+            self.Game.change(self.higher, 1)
 
+    def solve(self):
+        tries = 0
+        self.clear_midrow()
+        while not self.Game.check_win():
+            self.higher_lower()
+            self.higher_to_midrow()
+            if tries > 2*self.Game.number_disks:
+                break
+            tries += 1
+
+
+if __name__ == '__main__':
+    Game = HanoiTower()
+    Solve = SolveHanoiTower(Game)
+    Solve.solve()
     print(str(Game))
-    tries += 1
-    print(tries)
-    if tries > 2*Game.number_disks:
-        break
-
-print(Game.rods[1])
-print(Game.check_win())
+    print(Game.check_win())
+    print(Game.number_disks, Game.moves)
